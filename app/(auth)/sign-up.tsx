e799e53b908e44f30c82a0b8c14b338dab40e9f0
rahadul789 <haqrahadul@gmail.com>
2025-12-12 06:59:58 +0600

    User store in database

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constents";
+import { fetchAPI } from "@/lib/fetch";
 import { useSignUp } from "@clerk/clerk-expo";
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
@@ -58,6 +59,15 @@ const SignUp = () => {
       // and redirect the user
       if (signUpAttempt.status === "complete") {
         // TODO: Create a database user
+        await fetchAPI("/(api)/user", {
+          method: "POST",
+          body: JSON.stringify({
+            name: form.name,
+            email: form.email,
+            clerkId: signUpAttempt.createdUserId,
+          }),
+        });
+
         await setActive({ session: signUpAttempt.createdSessionId });
         setVerification({
           ...verification,
@@ -78,22 +88,6 @@ const SignUp = () => {
       });
     }
   };
-
-  // if (pendingVerification) {
-  //   return (
-  //     <>
-  //       <Text>Verify your email</Text>
-  //       <TextInput
-  //         value={code}
-  //         placeholder="Enter your verification code"
-  //         onChangeText={(code) => setCode(code)}
-  //       />
-  //       <TouchableOpacity onPress={onVerifyPress}>
-  //         <Text>Verify</Text>
-  //       </TouchableOpacity>
-  //     </>
-  //   );
-  // }
 
   return (
     <ScrollView className=" flex-1 bg-white">
